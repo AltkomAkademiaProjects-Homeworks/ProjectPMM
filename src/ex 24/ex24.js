bffdfae01ea6b1331c8baee0a21ebf1bba9edc96
pmmagusiak <58660110+pmmagusiak@users.noreply.github.com>
2019-12-21 09:43:29 +0100

    Update ex24.js

--- src/ex 24/ex24.js
+++ src/ex 24/ex24.js
@@ -4,7 +4,7 @@ class Person {
         this.surname = surname;
         this.age = age;
     }
-
+// gra w klasy
     displayNameAndSurname() {
         console.log("Imie: " + this.name + " Nazwisko: " + this.surname);
     }
